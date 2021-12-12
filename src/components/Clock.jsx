fa9a1e8210fd0f4e076892b97882c6551688e9cf
Christian Tejeda <christ.0@outlook.com>
2021-12-12 01:23:48 -0300

    Initial commit

---
+++ src/components/Clock.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Clock test.</p>
+            </div>
+        )
+    }
+}
